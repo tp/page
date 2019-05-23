022871d883330b094150f1b30dc5f7f7fca9a76d
Timm Preetz <timm@preetz.us>
2019-05-23 22:28:29 +0200

    Fix viewport

--- src/template.tsx
+++ src/template.tsx
@@ -58,6 +58,10 @@ function Head(props: HeaderProps) {
     <head>
       <meta httpEquiv="content-type" content="text/html;charset=utf-8" />
       <title>{props.title} | Timm Preetz</title>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no"
+      />
       <Style />
       <link rel="stylesheet" href="/assets/highlightjs.css" />
     </head>
