362fa596c3b5c2d9b59541509f02d94104ba0eee
Timm Preetz <timm@preetz.us>
2019-09-18 22:24:12 +0200

    Add icon

--- src/template.tsx
+++ src/template.tsx
@@ -63,6 +63,10 @@ function Head(props: HeaderProps) {
 
       <link rel="stylesheet" href="/assets/highlightjs.css" />
       <link rel="alternate" type="application/rss+xml" title="Timm Preetz's blog" href="/rss.xml" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/assets/favicon/site.webmanifest" />
     </head>
   );
 }
